315c59dc4772dba28b59b34a128edf5c0f712f9d
ADITYA PRASAD.S <70315903+adityaprasad502@users.noreply.github.com>
2021-08-25 10:49:44 +0530

    new commit
    from badcat
    
    Co-Authored-By: Md Jisan <66010486+jisan09@users.noreply.github.com>

--- userbot/plugins/tns.py
+++ userbot/plugins/tns.py
@@ -3,7 +3,7 @@ from userbot.utils import admin_cmd
 
 @bot.on(admin_cmd(incoming=True))
 async def _(event):
-    from_chnl = -1001562866430
+    from_chnl = -1001592228587
     if not event.is_private and event.chat_id == from_chnl:
-        target_chnl = -1001489788469
+        target_chnl = -1001445526942
         await event.client.send_message(target_chnl, event.message)
